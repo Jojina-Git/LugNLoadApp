70de0f16e5bd6dbf44e31f7e597a1d8df5b75ee7
Naveen Sivaraman Nair <naveensivaram1234@gmail.com>
2024-07-07 11:46:56 -0400

    Driver Request

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lugnload/UserDriverLogin.dart';
 import 'package:lugnload/Search.dart';
 import 'haul_form.dart';
 import 'firebase_options.dart';
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const UserDriverLogin(),
     );
   }
 }
